9d03bb9234b13f0fa51d11bdd154f61ff6f2f115
Francisco Romero <franciscoromero02@gmail.com>
2015-09-15 20:31:27 -0400

    Removed sample.cpp

--- sample.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main() {
-	std::cout << "sample repo" << std::endl;
-	std::cout << "This is another commit" << std::endl;
-	std::cout << "Change from github.com" << std::endl;
-	std::cout << "Testing reset" << std::endl;
-	return 0;
-}
