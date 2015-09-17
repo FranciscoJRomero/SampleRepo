7c3d541dbb95a881de3fb71f5adbf2944be248a1
Francisco Romero <franciscoromero02@gmail.com>
2015-09-17 14:39:11 -0400

    Change

--- sample.cpp
+++ sample.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+int sampleFunction() {
+	std::cout << "From Sample Function" << std::endl;
+}
 int main() {
 	std::cout << "Sample" << std::endl;
 	std::cout << "sample 2" << std::endl;
 	std::cout << "Code" << std::endl;
 	std::cout << "Test" << std::endl;
 	std::cout << "Test2" << std::endl;
+	sampleFunction();
 	return 0;
 }
